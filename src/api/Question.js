e471354df5f263e963c8e7a586dfba393e448077
osamahkh <khalili_86@hotmail.com>
2021-11-05 15:40:21 +0100

    update

---
+++ src/api/Question.js
@@ -0,0 +1,16 @@
+import axiosInstance from './axiosInstance';
+
+class Question {
+    static async getAll() {
+      return axiosInstance.get('/question/all').then((res) => res.data);
+    }
+    static async getByCategory(id) {
+        return axiosInstance.get('/question/category',{
+            params:{id:id}
+        }).then((res) => res.data);
+      }
+
+  }
+  
+  export default Question;
+  
